4e8979cebc471de422a86f9431ecca1fb1d65042
Dreamerydia <dreamerydia@gmail.com>
2015-03-13 00:36:42 -0400

    Flying System

--- src/server/scripts/Custom/anim.cpp
+++ src/server/scripts/Custom/anim.cpp
@@ -5,6 +5,8 @@
 #include "PoolMgr.h"
 #include "ObjectAccessor.h"
 #include "Transport.h"
+#include "Language.h"
+
 using namespace std;
 
 class anim : public CommandScript
@@ -18,6 +20,7 @@ public:
 		static ChatCommand animcommandTable[] =
 		{
 			{ "anim", SEC_PLAYER, false, &HandleAnimCommand, "", NULL },
+			{ "fly", SEC_PLAYER, false, &HandleFlyCommand, "", NULL },
 		};
 		return animcommandTable;
 	}
@@ -32,6 +35,31 @@ public:
 
 		return true;
 	}
+
+	static bool HandleFlyCommand(ChatHandler* handler, char const* args)
+	{
+		if (!*args)
+			return false;
+
+		Player* target = handler->getSelectedPlayer();
+		if (!target)
+			target = handler->GetSession()->GetPlayer();
+
+		WorldPacket data;
+
+		if (strncmp(args, "on", 3) == 0)
+			target->SetCanFly(true);
+		else if (strncmp(args, "off", 4) == 0)
+			target->SetCanFly(false);
+		else
+		{
+			handler->SendSysMessage(LANG_USE_BOL);
+			return false;
+		}
+		handler->PSendSysMessage(LANG_COMMAND_FLYMODE_STATUS, handler->GetNameLink(target).c_str(), args);
+		return true;
+	}
+
 };
 
 void AddSC_anim()
